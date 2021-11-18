53795751062a82fd39e5030c6cbf44f038669de7
palehazy <austinrpg@live.com>
2021-11-17 22:37:04 -0600

    adding kucoin api

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
 const BigNumber = require("bignumber.js");
 const { ethers } = require("ethers");
-const {commify, formatUnits} =ethers.utils
+const { commify, formatUnits } = ethers.utils;
+const https = require("https");
+const { config } = require("dotenv");
+config();
+const crypto = require("crypto");
+const KUCOIN_ENDPOINT = "api.kucoin.com";
+const PRICES = "/api/v1/prices";
 const DOBO_BNB_LP_ABI = require("./DOBO_BNB_LP_ABI.json");
 // const BSC_WEB_SOCKET_RPC = "wss://bsc-ws-node.nariox.org:443";
 const BSC_RPC = "https://bsc-dataseed.binance.org/";
 const BSC_NETWORK = 56;
+let BNB_PRICE = 580;
 // const DOBO_BNB_LP_ADDRESS = "0xBE80839a3BE4D3953D5588A60a11aEAED286b593";
 const BIRB_BNB_LP_ADDRESS = "0x2e5fE140C46aB86F8eD257c7831d131eBa74EF09";
 
-// let subscribeParams = {
-//     "id": 1545910660739,                          //The id should be an unique value
-//     "type": "subscribe",
-//     "topic": "/market/ticker:BTC-USDT,ETH-USDT",  //Topic needs to be subscribed. Some topics support to divisional subscribe the informations of multiple trading pairs through ",".
-//     "privateChannel": false,                      //Adopted the private channel or not. Set as false by default.
-//     "response": true                              //Whether the server needs to return the receipt information of this subscription or not. Set as false by default.
-// }
-// var socket = new WebSocket("wss://push1-v2.kucoin.com/endpoint?token=xxx&[connectId=xxxxx]");
-// // let doboAmount = "19061801959701464822611";
-// let doboDecimals = 9;
+function getBNBPriceFromKucoin() {
+  return new Promise((resolve) => {
+    const timestamp = Date.now();
+    const signatureData = `${timestamp}GET${PRICES}`;
+    const signatureAPI = crypto
+      // eslint-disable-next-line no-undef
+      .createHmac("sha256", process.env.API_SECRET)
+      .update(signatureData)
+      .digest("base64");
+    const signatureAPI_PASS = crypto
+      // eslint-disable-next-line no-undef
+      .createHmac("sha256", process.env.API_SECRET)
+      // eslint-disable-next-line no-undef
+      .update(process.env.PASS)
+      .digest("base64");
 
-// let dobo = BigNumber(doboAmount);
-// dobo.shiftedBy(-doboDecimals);
+    const reqOptions = {
+      hostname: KUCOIN_ENDPOINT,
+      port: 443,
+      path: PRICES + "?currencies=BNB",
+      method: "GET",
+      headers: {
+        // eslint-disable-next-line no-undef
+        KC_API_KEY: process.env.API_KEY,
+        KC_API_SIGN: signatureAPI,
+        "KC-API-TIMESTAMP": timestamp,
+        "KC-API-PASSPHRASE": signatureAPI_PASS,
+        "KC-API-KEY-VERSION": "2",
+      },
+    };
+    const req = https.request(reqOptions, (res) => {
+      res.on("data", (d) => {
+        // const x = d;
+        resolve(d.toString());
+      });
+    });
+    req.end();
+  });
+}
 
-// // let x = 19001095378833240382345n;
-
-// let wbnbAmount = "5131349111279551121689";
-// let wbnb = BigNumber(wbnbAmount);
-// let wbnbDecimals = 18;
-// wbnb.shiftedBy(-wbnbDecimals);
-
-// // console.log(doboAmount < x)
-
-// console.log(dobo.toString());
-// console.log(wbnb.toString());
-// // need to figure out the math as to why we are shifting by 9
-// let doboForOneBNB = dobo.dividedBy(wbnb).shiftedBy(doboDecimals);
-// console.log(doboForOneBNB.toString());
-// let bnbPriceInDollars = 633.06;
-// bnbPriceInDollars = BigNumber(bnbPriceInDollars);
-// let bnbAmountForOneDollar = BigNumber(1).dividedBy(bnbPriceInDollars);
-
-// // console.log(bnbAmountForOneDollar.toString());
-
-// let amountOfDoboForOneDollar = doboForOneBNB.multipliedBy(
-//   bnbAmountForOneDollar
-// );
-
-// // console.log(amountOfDoboForOneDollar.toString());
-
-// let dollarAmountForOneDobo = BigNumber(1)
-//   .shiftedBy(9)
-//   .dividedBy(amountOfDoboForOneDollar);
-// console.log(dollarAmountForOneDobo.shiftedBy(-9).toFixed(9));
-// console.log(doboForOneBNB.shiftedBy(9).toString())
-// console.log(dobo)
-// console.log(doboAmount.toNumber())
-// console.log(wbnbAmount)
 function getDollarAmountForOneToken(
   token1Amount,
   token1Decimals,
@@ -63,69 +62,99 @@ function getDollarAmountForOneToken(
   token2Decimals,
   BNBPrice
 ) {
+  token1Amount = BigNumber(token1Amount);
+  console.log(token1Amount);
+  token1Amount.shiftedBy(-token1Decimals);
+  console.log("token 1 reserves:", token1Amount.toString());
+  token2 = BigNumber(token2);
+  token2.shiftedBy(-token2Decimals);
+  console.log("token 2 reserves:", token2.toString());
+  // need to figure out the math as to why we are shifting by 9 it is probably unnecessary
+  let tokenOneForOneTokenTwo = token1Amount
+    .dividedBy(token2)
+    .shiftedBy(token1Decimals);
+  console.log(tokenOneForOneTokenTwo.toString());
+  BNBPrice = BigNumber(BNBPrice);
+  let bnbAmountForOneDollar = BigNumber(1).dividedBy(BNBPrice);
 
-token1Amount = BigNumber(token1Amount);
-console.log('token 1 reserves:', token1Amount.toString());
-token1Amount.shiftedBy(-token1Decimals);
-token2 = BigNumber(token2);
-token2.shiftedBy(-token2Decimals);
-console.log('token 2 reserves:', token2.toString());
-// need to figure out the math as to why we are shifting by 9 it is probably unnecessary
-let tokenOneForOneTokenTwo = token1Amount.dividedBy(token2).shiftedBy(token1Decimals);
-console.log(tokenOneForOneTokenTwo.toString());
-BNBPrice = BigNumber(BNBPrice);
-let bnbAmountForOneDollar = BigNumber(1).dividedBy(BNBPrice);
+  // console.log(bnbAmountForOneDollar.toString());
 
-// console.log(bnbAmountForOneDollar.toString());
+  let amountOfTokenOneForOneDollar = tokenOneForOneTokenTwo.multipliedBy(
+    bnbAmountForOneDollar
+  );
 
-let amountOfTokenOneForOneDollar = tokenOneForOneTokenTwo.multipliedBy(
-  bnbAmountForOneDollar
+  // console.log(amountOfDoboForOneDollar.toString());
+
+  let dollarAmountForOneTokenOne = BigNumber(1)
+    .shiftedBy(9)
+    .dividedBy(amountOfTokenOneForOneDollar);
+  return dollarAmountForOneTokenOne.shiftedBy(-9).toFixed(15);
+}
+
+const provider = new ethers.providers.JsonRpcProvider(BSC_RPC, BSC_NETWORK);
+const DOBO_WBNB_LP_CONTRACT = new ethers.Contract(
+  // DOBO_BNB_LP_ADDRESS,
+  BIRB_BNB_LP_ADDRESS,
+  DOBO_BNB_LP_ABI,
+  provider
 );
+async function callReserves() {
+  let bnbPrice = await getBNBPriceFromKucoin();
+  bnbPrice = JSON.parse(bnbPrice);
+  BNB_PRICE = bnbPrice.data.BNB;
+  const reserves = await DOBO_WBNB_LP_CONTRACT.getReserves();
 
-// console.log(amountOfDoboForOneDollar.toString());
-
-let dollarAmountForOneTokenOne = BigNumber(1)
-  .shiftedBy(9)
-  .dividedBy(amountOfTokenOneForOneDollar);
-return dollarAmountForOneTokenOne.shiftedBy(-9).toFixed(15);
+  const amount = getDollarAmountForOneToken(
+    reserves[0].toString(),
+    9,
+    reserves[1].toString(),
+    18,
+    BNB_PRICE
+  );
+  console.log("price per one ", amount);
+  return amount;
 }
 (async () => {
   // const provider = new ethers.providers.WebSocketProvider(
   //     BSC_WEB_SOCKET,
   //     BSC_NETWORK
   //   );
-  let bnbDollars = 574.55;
-  const provider = new ethers.providers.JsonRpcProvider(BSC_RPC, BSC_NETWORK);
-  const DOBO_WBNB_LP_CONTRACT = new ethers.Contract(
-    // DOBO_BNB_LP_ADDRESS,
-    BIRB_BNB_LP_ADDRESS,
-    DOBO_BNB_LP_ABI,
-    provider
-  );
-  const reserves = await DOBO_WBNB_LP_CONTRACT.getReserves();
-//   console.log(reserves[0]);
-//   console.log(reserves[1]);
-//   console.log(reserves[0].shl(9).toString());
-//   console.log(reserves[1].shl(18).toString());
-  const amount = getDollarAmountForOneToken(
-    reserves[0].toString(),
-    9,
-    reserves[1].toString(),
-    18,
-    bnbDollars
-  );
-  console.log('price per one ', amount)
 
- DOBO_WBNB_LP_CONTRACT.on('Swap', (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
-      console.log('swap occured')
-      console.log('sender:', sender)
-      console.log('amount0In (BIRB):', commify(formatUnits(amount0In, 9)))
-      console.log('amount1In (WBNB):', commify(formatUnits(amount1In, 18)))
-      console.log('amount0Out (BIRB):', commify(formatUnits(amount0Out, 9)))
-      console.log('amount1Out: (WBNB)', commify(formatUnits(amount1Out, 18)))
-      console.log('to:', to)
-      
-  })
+  let amount = await callReserves();
+  DOBO_WBNB_LP_CONTRACT.on(
+    "Swap",
+    async (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
+      console.log("swap occured");
+      console.log("sender:", sender);
+      amount0In = new BigNumber(formatUnits(amount0In, 9));
+      console.log(
+        "amount0In (BIRB):",
+        commify(amount0In.toString()),
+        "$" + amount0In.multipliedBy(amount).toString()
+      );
+      amount1In = new BigNumber(formatUnits(amount1In, 18));
+      console.log(
+        "amount1In (WBNB):",
+        commify(amount1In.toString()),
+        "$" + amount1In.multipliedBy(BNB_PRICE).toString()
+      );
+      amount0Out = new BigNumber(formatUnits(amount0Out, 9));
+      console.log(
+        "amount0Out (BIRB):",
+        commify(amount0Out.toString()),
+        "$" + amount0Out.multipliedBy(amount).toString()
+      );
+      amount1Out = new BigNumber(formatUnits(amount1Out, 18));
+      console.log(
+        "amount1Out: (WBNB)",
+        commify(amount1Out.toString()),
+        "$" + amount1Out.multipliedBy(BNB_PRICE).toString()
+      );
+      console.log("to:", to);
+      console.log("new price");
+      amount = await callReserves();
+    }
+  );
   // ethers.BigNumber.from(42).shl()
   // console.log(ethers.BigNumber.from(reserves[0]).toString())
   // console.log(ethers.BigNumber.from(reserves[1]).toString())
